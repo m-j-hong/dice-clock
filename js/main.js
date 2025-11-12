3d3c66fbefa0fb91938d81081ab825348d41bf82
MJH Work <hong.m.j@wustl.edu>
2025-11-12 00:10:15 -0600

    viewport size responsiveness

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const now = new Date();
-const month = now.getMonth();
+const month = now.getMonth() + 1;
 const date = now.getDate();
 const hours = now.getHours();
 const minutes = now.getMinutes();
@@ -48,49 +48,59 @@ let minuteC = document.getElementById('minute-c')
 
 let hCountA, hCountB, mCountA, mCountB, mCountC
 
-console.log(hours)
-let h12 = hours % 12
+// set 24 hour scale to 12
+// 0 means 12, otherwise regular
+let h12 = (hours == 0) ? 12 : hours % 12
 
+console.log(hours)
 console.log(minutes)
 console.log(month)
 console.log(date)
 
+// split hours equally among 2 dice
 hCountA = h12 - Math.ceil(h12 / 2)
 hCountB = Math.ceil(h12 / 2)
 
+// tens of minutes
 mCountA = Math.floor(minutes / 10)
+
+// ones of minutes split among 2 dice
 mCountB = (minutes % 10) - Math.ceil((minutes % 10) / 2)
 mCountC = Math.ceil((minutes % 10) / 2)
 
-let shadowX, shadowY
-
 function updateDice() {
+    // light/dark theme
     (hours < 6 || hours >= 14) ? document.body.style.background = '#28232f' : document.body.style.background = '#f8f7f6'
+    
+    // shadows
+    // 0 - 24, negative from 24 to 12, positive from 12 to 24. maximum at 6 and 18
+    let shadowX, shadowY
     if (0 <= hours && hours < 6) {
         console.log('q1')
-        shadowX = -(hours % 6) * 5
-        shadowY = ((hours % 6) - 6) * 5
+        shadowX = -(hours % 6) / 2
+        shadowY = ((hours % 6) - 6) / 2
     }
     else if (6 <= hours && hours < 12) {
         console.log('q2')
-        shadowX = ((hours % 6) - 6) * 5
-        shadowY = (hours % 6) * 5
+        shadowX = ((hours % 6) - 6) / 2
+        shadowY = (hours % 6) / 2
     }
     else if (12 <= hours && hours < 18) {
         console.log('q3')
-        shadowX = (hours % 6) * 8
-        shadowY = (6 - (hours % 6)) * 5
+        shadowX = (hours % 6) / 2
+        shadowY = (6 - (hours % 6)) / 2
     }
     else {
         console.log('q4')
-        shadowX = (6 - (hours % 6)) * 5
-        shadowY = -(hours % 6) * 5
+        shadowX = (6 - (hours % 6)) / 2
+        shadowY = -(hours % 6) / 2
     }
-    let insetShadow = shadowX/5 + 'px ' + shadowY/5 + 'px 0 0 #00000080 inset';
-    let dieShadow = shadowX + 'px ' + shadowY + 'px 0 0 #00000080';
 
+    // shadow size
+    let insetShadow = shadowX/8 + 'vw ' + shadowY/6 + 'vw 0 0 #00000080 inset';
+    let dieShadow = shadowX + 'vw ' + shadowY + 'vw 0 0 #00000080';
 
-    // 0 - 24, negative from 24 to 12, positive from 12 to 24. maximum at 6 and 18
+    // set # of pips on each die
     hourA.innerHTML = numbers[hCountA]
     hourB.innerHTML = numbers[hCountB]
     minuteA.innerHTML = numbers[mCountA]
@@ -99,13 +109,16 @@ function updateDice() {
 
     for (const d of allDice) {
         if (d.innerHTML == numbers[0]) {
+            // if # is 0, looks like die isn't there
             d.style.boxShadow =  insetShadow
         }
         else {
+            // regular positive shadow
             d.style.boxShadow =  dieShadow
         }
     }
 
+    // inset shadows
     for (const c of colonDots) {
         c.style.boxShadow =  insetShadow
     }
